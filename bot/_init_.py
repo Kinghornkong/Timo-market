daf2013c9d6d94612e6512bdf5106cd82a5b5f5f
Kinghornkong <tonyishmael8@yahoo.com>
2025-07-18 17:54:24 +0300

    Update _init_.py

--- bot/_init_.py
+++ bot/_init_.py
@@ -1,9 +1,9 @@
 bot/__init__.py
 # Initializes Bot and Loads Handlers
-# =========================
+# ***p****o*****p*****e****
 
 import os
 from telebot import TeleBot
 from . import handlers, admin, mpesa
 
-bot = TeleBot(os.environ.get("7964013672:AAH_8NKlDZQxG590rH9SP30tR65-l687iso"))
+bot = TeleBot(os.environ.get("<add bot api key here!"))
